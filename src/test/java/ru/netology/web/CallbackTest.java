ce37a04764b320ccb1828b70c8a7094c6b9d2ea6
Slava netologia <s-29@mail.ru>
2024-08-28 10:22:19 +0300

    Fix test

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -2,6 +2,7 @@ package ru.netology.web;
 
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -14,15 +15,20 @@ import static com.codeborne.selenide.Selenide.open;
 class CallbackTest {
     @Test
     void shouldTest() {
+        String date = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=city] input").setValue("Москва");
-        form.$("[data-test-id=date] input").setValue(LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+        form.$("[data-test-id=date] input").doubleClick();
+        form.$("[data-test-id=date] input").sendKeys(Keys.DELETE);
+        form.$("[data-test-id=date] input").setValue(date);
         form.$("[data-test-id=name] input").setValue("Иван Иванов");
         form.$("[data-test-id=phone] input").setValue("+79110000000");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
         $("[data-test-id=notification]").shouldBe(visible,Duration.ofSeconds(15));
+        $("[data-test-id=notification]").shouldHave(text(date));
+
     }
 }
 
